5b6dd8dd1ae969d32fbcc1a72e59b66ca2c605c8
TuralValizade <turalvalizada@mail.ru>
2026-06-06 08:29:38 +0400

    Fix: Scroll to top on route change

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
+import ScrollToTop from './components/ScrollToTop'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </HelmetProvider>
